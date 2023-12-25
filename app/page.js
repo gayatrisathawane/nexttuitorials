bd4c9d224cf8d3dd843aa0bef6a9af16fb0298ab
gayatrisathawane <gayatripsathawane@gmail.com>
2023-12-25 23:33:32 +0530

    component,event,state,function

--- app/page.js
+++ app/page.js
@@ -8,6 +8,14 @@ export default function Home() {
   const apple = ()=>{
     setName("aakansha dahekar")
   }
+
+  const InnerComponet =()=>{
+    return(
+     <div>
+      <h1>Inner demo</h1>
+     </div>
+    )
+  }
    
   return (
     <main className="min-h-screen flex-col items-center justify-between p-24">
@@ -16,6 +24,8 @@ export default function Home() {
         <button onClick={()=>{
          apple()
         }}>Click me </button>
+        <InnerComponet/>
+        {InnerComponet()}
       </div>
     </main>
   )
